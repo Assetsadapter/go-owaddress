f7cc155e4830a9c2d985c4b3bf8d221bbcfd4899
finley0916 <55075717+finley0916@users.noreply.github.com>
2019-10-29 13:39:31 +0800

    Add files via upload

---
+++ test/etz_test.go
@@ -0,0 +1,42 @@
+package test
+
+import (
+	"github.com/Assetsadapter/go-owaddress"
+	"testing"
+)
+
+func Test_etz_AddressVerify_Valid(t *testing.T) {
+
+	coin := "etz"
+	expect := true
+
+	Address := "0x97edc8c4e5ca09fa5094d24639e09947fe39829a23"
+
+	valid, err := owaddress.Verify(coin, Address)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	if valid != expect {
+		t.Error("Failed to verify valid address")
+	}
+}
+
+func Test_etz_AddressVerify_InValid(t *testing.T) {
+
+	coin := "etz"
+	expect := false
+
+	Address := "0xldcbc4eac58965d9d845442df859a2f5434fec7a"
+
+	valid, err := owaddress.Verify(coin, Address)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	if valid != expect {
+		t.Error("Failed to verify invalid address")
+	}
+}
